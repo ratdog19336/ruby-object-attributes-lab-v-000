2b349f0d6545accaf1761c50f4275f6c771e35fb
Nicholas Ratti <nickratti@gmail.com>
2016-09-19 10:32:08 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,9 @@
+class Dog
+  attr_accessor :name, :breed
+end
+
+fido = Dog.new
+fido.name = "Fido"
+
+snoopy = Dog.new
+snoopy.name = "Snoopy"
---
+++ lib/person.rb
@@ -0,0 +1,6 @@
+class Person
+  attr_accessor :name, :job
+end
+
+person = Person.new
+person.name = "Person"
